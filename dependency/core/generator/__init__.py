0416222a922b0fe84a87f3bac8051552d0d6e46b
fanl0228 <1430337978@qq.com>
2025-09-18 16:37:33 +0800

    Add mmw_detection#1

--- dependency/core/generator/__init__.py
+++ dependency/core/generator/__init__.py
@@ -2,3 +2,4 @@ from .generator_server import GeneratorServer
 from .video_generator import VideoGenerator
 from .audio_generator import AudioGenerator
 from .imu_generator import IMUGenerator
+from .radio_generator import RadioGenerator
