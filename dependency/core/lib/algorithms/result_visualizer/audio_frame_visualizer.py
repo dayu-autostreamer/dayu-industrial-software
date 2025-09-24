29f91972e9e7afea91a31d522f8870d91ebc5d85
onecheck <1548767361@qq.com>
2025-09-25 01:53:50 +0800

    fix: add chinese tiff to matplotlib

--- dependency/core/lib/algorithms/result_visualizer/audio_frame_visualizer.py
+++ dependency/core/lib/algorithms/result_visualizer/audio_frame_visualizer.py
@@ -19,7 +19,7 @@ class AudioFrameVisualizer(ImageVisualizer, abc.ABC):
         self.audio_service = kwargs.get('audio_service', None)
 
         self.sound_categories = [
-            "Other (其他声音)"
+            "Other (其他声音)",
             "Car Horn (喇叭声)",
             "Drilling (钻孔声)",
             "Engine Idling (引擎声)",
@@ -35,7 +35,7 @@ class AudioFrameVisualizer(ImageVisualizer, abc.ABC):
 
         import librosa
         import matplotlib.pyplot as plt
-        plt.rcParams['font.family'] = 'SimHei'
+        plt.rcParams['font.family'] = 'Noto Sans CJK SC'
 
         databuffer = np.frombuffer(data, dtype=np.short)
         if nchannels == 2:
