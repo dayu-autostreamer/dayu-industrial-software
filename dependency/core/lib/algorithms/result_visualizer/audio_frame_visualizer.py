5aa698aba13d8caf97ae513d14776d4f643def4e
onecheck <1548767361@qq.com>
2025-09-30 01:00:17 +0800

    feat: accelerate audio visualization

--- dependency/core/lib/algorithms/result_visualizer/audio_frame_visualizer.py
+++ dependency/core/lib/algorithms/result_visualizer/audio_frame_visualizer.py
@@ -42,7 +42,7 @@ class AudioFrameVisualizer(ImageVisualizer, abc.ABC):
 
         # Persistent Matplotlib objects
         self._plt = plt
-        self.fig = plt.figure(figsize=(4,2),dpi=200)
+        self.fig = plt.figure(figsize=(4,4),dpi=200)
         self.canvas = FigureCanvas(self.fig)
         self.ax = self.fig.add_subplot(111)
 
@@ -56,8 +56,6 @@ class AudioFrameVisualizer(ImageVisualizer, abc.ABC):
         self.ax.set_xlabel("Time (s)")
         self.ax.set_ylabel("Frequency (Hz)")
         self.ax.set_title("")  # will set per-call
-        self.fig.tight_layout(pad=1.0)
-        self.fig.subplots_adjust(top=1.0)
         self.ax.set_xticks([])
         self.ax.set_yticks([])
 
