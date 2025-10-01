6d32d0b61d1c10ded4affd090edd80a7fd83c0df
ItzDesert <itzdesert123@gmail.com>
2025-10-02 02:06:33 +0800

    增加其他模态告警

---
+++ dependency/core/lib/algorithms/event_trigger/audio_trigger.py
@@ -0,0 +1,20 @@
+import abc
+import numpy as np
+from core.lib.common import ClassFactory, ClassType, EncodeOps, LOGGER
+from core.lib.content import Task
+from .base_trigger import BaseTrigger
+
+__all__ = ('AudioTrigger',)
+
+
+@ClassFactory.register(ClassType.EVENT_TRIGGER, alias='audio')
+class AudioTrigger(BaseTrigger, abc.ABC):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __call__(self, task: Task):
+        # 返回各阶段的详细用时
+        if task.get_last_content() == 5:
+            return True, {}
+        return False, {}
+
